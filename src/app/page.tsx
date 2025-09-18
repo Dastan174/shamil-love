59675ff274fd847decf00d5f26851e86eadfad83
Dastan174 <kanbolotuuludas@gmail.com>
2025-09-18 15:52:25 +0600

    .

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,12 +7,35 @@ import Trip from "../components/trip/Trip";
 import LoveCard from "../components/loveCard/LoveCard";
 import Chat from "../components/chat/Chat";
 import LastChat from "../components/lastChat/LastChat";
-import { useState } from "react";
+import { useEffect, useRef, useState } from "react";
 
 export default function Home() {
   const [onOpen, setOnOpen] = useState(false);
+  const audioRef = useRef(null);
+  useEffect(() => {
+    const enableSound = () => {
+      if (audioRef.current) {
+        audioRef.current.muted = false; // снимаем mute
+        audioRef.current
+          .play()
+          .catch((e) => console.log("Ошибка воспроизведения:", e));
+      }
+    };
+    document.addEventListener("click", enableSound, { once: true });
+    document.addEventListener("scroll", enableSound, { once: true });
+    document.addEventListener("keydown", enableSound, { once: true });
+    return () => {
+      document.removeEventListener("click", enableSound);
+      document.removeEventListener("scroll", enableSound);
+      document.removeEventListener("keydown", enableSound);
+    };
+  }, []);
   return (
     <div className={styles.page}>
+      <audio ref={audioRef} autoPlay loop muted>
+        <source src="/assets/shama-song.mp3" type="audio/mpeg" />
+        Ваш браузер не поддерживает аудио.
+      </audio>
       <LoveCard onOpen={() => setOnOpen(true)} />
       {/* {onOpen && ( */}
       <>
