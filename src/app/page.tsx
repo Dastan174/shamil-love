65b7f696477e149545be4653d2a1a8c0f0e16ec5
Dastan174 <kanbolotuuludas@gmail.com>
2025-09-18 16:40:07 +0600

    .

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -33,7 +33,7 @@ export default function Home() {
   return (
     <div className={styles.page}>
       <audio ref={audioRef} autoPlay loop>
-        <source src="/assets/songv.mp3" type="audio/mpeg" />
+        <source src="/shamaSong.mp3" type="audio/mpeg" />
         Ваш браузер не поддерживает аудио.
       </audio>
       <LoveCard onOpen={() => setOnOpen(true)} />
